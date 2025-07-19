723601ef5e9e1b08cdf824a03cb360919c8f91d2
Yash Kumar Verma <yashkumarverma623@gmail.com>
2025-07-20 00:14:14 +0530

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,21 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-// Email validation
 const validateEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
-// Error handler
 const handleError = (res, error, message = 'Something went wrong') => {
   console.error('Error:', error);
   res.status(500).json({ error: message });
 };
 
-// JWT authentication middleware
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+  const token = authHeader && authHeader.split(' ')[1]; 
 
   if (!token) {
     return res.status(401).json({ error: 'Access token required' });
